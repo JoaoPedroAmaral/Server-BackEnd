87ec1656305e78667dc522edac2554817c04d2e8
joão pedro <joaopedro251900@gmail.com>
2024-11-23 12:08:53 -0300

    mongo

--- server.js
+++ server.js
@@ -1,12 +1,53 @@
 //Dependencia para criar servidores
 import express from "express";
 
+const posts = [
+    {
+        id:1,
+        descrição: "Um cachorro cavando",
+        imagem: "https://http.dog/404.jpg" // Substitua o código de status por outros para variar as imagens
+    },
+    {
+        id:2,
+        descrição: "Cachorrinho feliz",
+        imagem: "https://http.dog/200.jpg"
+    },
+    {
+        id:3,
+        descrição: "dog sendo puxado",
+        imagem: "https://http.dog/500.jpg"
+    },
+    {
+        id:4,
+        descrição: "cachorro se cheirando",
+        imagem: "https://http.dog/304.jpg"
+    },
+    {
+        id:5,
+        descrição: "Cães dormindo",
+        imagem: "https://http.dog/201.jpg"
+    },
+    {
+        id:6,
+        descrição: "Um cachorro em cima de um bulling",
+        imagem: "https://http.dog/418.jpg" // Código de status "I'm a teapot" para uma imagem divertida
+    }
+  ];
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000, ()=> {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req,res) => {
-    res.status(200).send("Bem vindo ao servidor"); //200 é http status code de sucesso
+function buscapost(id){
+    return posts.findIndex((post)=>{
+        return post.id === Number(id);
+    })
+}
+
+app.get("/post/:id", (req,res) => {
+    const index = buscapost(req.params.id);
+    res.status(200).send(posts[index]); //200 é http status code de sucesso
 });
---
+++ src/config/dbConfig.js
@@ -0,0 +1,17 @@
+import { MongoClient } from "mongodb";
+
+export default async function conectarAoBanco(stringConexao) {
+    let mongoClient;
+
+    try{
+        mongoClient = new MongoClient (stringConexao);
+        console.log('conectando ao cluster do bd');
+        await mongoClient.connect();
+        console.log('Conectando ao MongoDB Atlas com o servidor');
+
+        return mongoClient;
+    }catch(erro){
+        console.log('falha na conexao com o banco!');
+        process.exit();
+    }
+}
